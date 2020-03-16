4a3fce0d285def58ae450ebf2205045c145fafe5
Pravin <patelpra337@gmail.com>
2020-03-16 18:21:23 -0500

    add project, create storyboard, add viewControllers

---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by patelpra on 3/16/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
